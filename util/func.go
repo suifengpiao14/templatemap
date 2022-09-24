c184a60eaec901cd3d152b2d5b26b78e7dd7dadc
pz <2912150017@qq.com>
2022-09-24 13:18:32 +0800

    optimize

--- util/func.go
+++ util/func.go
@@ -127,7 +127,39 @@ func GetTemplateNames(t *template.Template) []string {
 	return out
 }
 
-func Validate(input string, jsonLoader gojsonschema.JSONLoader) error {
+func Validate(input string, jsonLoader gojsonschema.JSONLoader) (err error) {
+	if input == "" {
+		jsonschema, err := jsonLoader.LoadJSON()
+		if err != nil {
+			return err
+		}
+		jsonMap, ok := jsonschema.(map[string]interface{})
+		if !ok {
+			err = errors.Errorf("can not convert jsonLoader.LoadJSON() to map[string]interface{}")
+			return err
+		}
+		typ, ok := jsonMap["type"]
+		if !ok {
+			err = errors.Errorf("jsonschema missing property type :%v", jsonschema)
+			return err
+		}
+		typStr, ok := typ.(string)
+		if !ok {
+			err = errors.Errorf("can not convert  jsonschema type to string :%v", typ)
+			return err
+
+		}
+		switch strings.ToLower(typStr) {
+		case "object":
+			input = "{}"
+		case "array":
+			input = "[]"
+		default:
+			err = errors.Errorf("invalid jsonschema type:%v", typStr)
+			return err
+		}
+
+	}
 	documentLoader := gojsonschema.NewStringLoader(input)
 	result, err := gojsonschema.Validate(jsonLoader, documentLoader)
 	if err != nil {
